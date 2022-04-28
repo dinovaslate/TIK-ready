fda5c36b955b8550f2ef503c0d5a407b871c3c93
unknown <dinovasala3@gmail.com>
2022-04-28 11:09:52 +0700

    final commit

--- src/index.js
+++ src/index.js
@@ -123,7 +123,6 @@ slider.addEventListener("mouseup", () => {
 slider.addEventListener("mousemove", (e) => {
   if (!isDown) return;
   e.preventDefault();
-  prevScroll = slider.scrollLeft;
   const x = e.pageX - slider.offsetLeft;
   const walk = (x - startX) * 3;
   slider.scrollLeft = scrollLeft - walk;
